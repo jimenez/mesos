f52f3d664e9d447b3d466ad0193785420c822c35
Isabel Jimenez <contact@isabeljimenez.com>
2015-09-21 14:58:03 -0400

    validations for call on the slave

---
+++ src/slave/validation.cpp
@@ -0,0 +1,81 @@
+`/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <string>
+#include <vector>
+
+#include <glog/logging.h>
+
+#include "slave/validation.hpp"
+
+using std::string;
+using std::vector;
+
+namespace mesos {
+namespace internal {
+namespace slave {
+namespace validation {
+namespace executor {
+namespace call {
+
+Option<Error> validate(const mesos::executor::Call& call)
+{
+  if (!call.IsInitialized()) {
+    return Error("Not initialized: " + call.InitializationErrorString());
+  }
+
+  // All calls should have framework id set.
+  if (!call.has_framework_id()) {
+    return Error("Expecting 'framework_id' to be present");
+  }
+
+  // All calls should have executor id set.
+  if (!call.has_executor_id()) {
+    return Error("Expecting 'executor_id' to be present");
+  }
+
+  switch (call.type()) {
+    case mesos::executor::Call::SUBSCRIBE:
+      if (!call.has_subscribe()) {
+        return Error("Expecting 'subscribe' to be present");
+      }
+      return None();
+
+    case mesos::executor::Call::UPDATE:
+      if (!call.has_update()) {
+        return Error("Expecting 'update' to be present");
+      }
+      return None();
+
+    case mesos::executor::Call::MESSAGE:
+      if (!call.has_message()) {
+        return Error("Expecting 'message' to be present");
+      }
+      return None();
+
+    default:
+      return Error("Unknown call type");
+  }
+}
+
+} // namespace call {
+} // namespace executor {
+} // namespace validation {
+} // namespace slave {
+} // namespace internal {
+} // namespace mesos {
